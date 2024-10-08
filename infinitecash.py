f4015c4902b7655a197140122c95422e82492fa3
Simon <simonwa01@gmail.com>
2024-10-08 16:11:35 +0200

    ✨ NEW: Initial solution for infinitecash

---
+++ infinitecash.py
@@ -0,0 +1,31 @@
+s = int(input(), base=2)
+d = int(input(), base=2)
+m = int(input(), base=2)
+cnt = 0
+initial_m = m
+previous_m = m
+
+while (m > 0):
+    if m >= initial_m and cnt != 0: 
+        # maype equal
+        print("Infinite money!")
+        exit(0)
+        break
+    
+    m = m >> 1
+
+    cnt += 1
+
+    if cnt % d == 0:
+        m += s
+        if (previous_m == m):
+            print("Infinite money!")
+            exit(0)
+            break
+        previous_m = m
+
+
+ret = f'{cnt:#b}'
+ret = ret.replace('0b', '')
+
+print(f'{ret}')
